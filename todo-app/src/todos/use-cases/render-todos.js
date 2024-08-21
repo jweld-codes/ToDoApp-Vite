70405fb836514f3a97a4df164e24d069fed14588
jweld-codes <jemima.weld@studentambassadors.com>
2024-08-20 21:21:06 -0600

    validaciones y optimizaciones

--- todo-app/src/todos/use-cases/render-todos.js
+++ todo-app/src/todos/use-cases/render-todos.js
@@ -3,6 +3,8 @@ import { Todo } from '../models/todo.models';
 import { createTodoHTML } from './create-todo-html';
 
 
+let element;
+
 /**
  * 
  * @param {String} elemntId 
@@ -11,11 +13,16 @@ import { createTodoHTML } from './create-todo-html';
 
 export const renderTodos = ( elemntId, todos = [] ) => {
 
-    const element = document.querySelector( elemntId );
+    if(!element)
+        element = document.querySelector( elemntId );
 
-    todos.forEach(todo => {
-        element.append( createTodoHTML(todo))
-    });
+        if(!element) throw new error(`Element ${ elemntId} not found`);
+
+        element.innerHTML = '';
+
+        todos.forEach(todo => {
+            element.append( createTodoHTML(todo))
+        });
 
 
 }
