b720d03fbf2e8168c8914ab14a1ffc42acf1ed55
jweld-codes <jemima.weld@studentambassadors.com>
2024-08-20 20:38:04 -0600

    implementacion de los metodos en store

--- todo-app/src/store/todo.store.js
+++ todo-app/src/store/todo.store.js
@@ -21,7 +21,71 @@ const initStore = () => {
     console.log('InitStore');
 }
 
+const loadStore = () => {
+    throw new Error('No implementado');
+}
+
+const getTodo = (filer = Filters.All) => {
+    switch(filter){
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+        default:
+            throw new Error(`Option ${ fliter } is not valid.`);
+
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo =(description) => {
+    if (!description) throw new Error('La descripcion es obligatoria');
+
+    state.todos.push(new Todo(description));
+}
+
+const toggleTodo = (todoId) => {
+    state.todos = state.todos.map( todo => {
+        if( todo.id === todoId ) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+}
+
+const deleteTodo = (todoId) => {
+    state.todos = state.todos.filter(todo => todo.id !== todoId);
+}
+
+const deleteAllTodo = () => {
+    state.todos = state.todos.filter(todo => todo.done);
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+    state.filter = newfilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 export default {
     initStore,
+    getTodo,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    deleteAllTodo,
+    setFilter,
+    getCurrentFilter,
 }
 
